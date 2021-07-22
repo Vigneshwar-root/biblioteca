a0321807616404ac25d8c277f15ca2ed27fd5d43
Vigneshwar RK <rkvfcvemb@gmail.com>
2021-07-22 22:25:25 +0530

    Installed angular material,bootstrap5 and created login component

--- la-biblioteca/src/app/app.module.ts
+++ la-biblioteca/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
